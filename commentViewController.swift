8bee828545e47f402a9832723cfcf8d884a0ffcc
micksatit458 <micktoon913@gmail.com>
2020-04-02 16:02:22 +0700

    uu

---
+++ commentViewController.swift
@@ -0,0 +1,30 @@
+//
+//  commentViewController.swift
+//  tradeFinal
+//
+//  Created by Satit Nangnoi on 2/4/2563 BE.
+//  Copyright © 2563 Satit Nangnoi. All rights reserved.
+//
+
+import UIKit
+
+class commentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
